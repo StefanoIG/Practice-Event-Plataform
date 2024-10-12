142d0d500821bfc4f51d8c2c4c1fd82e9cb98cbd
StefanoIG <stefano20033@gmail.com>
2024-10-12 13:48:13 -0500

    Refactor routing configuration and remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistroForm from './components/UX/Pages/RegistroForm';
 import LoginForm from './components/UX/Pages/LoginForm';
