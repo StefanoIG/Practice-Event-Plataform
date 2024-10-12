4ae23c0c3ff9e344424779ad9392b89992655e18
StefanoIG <stefano20033@gmail.com>
2024-10-12 13:59:41 -0500

    Refactor import statement in main.tsx and remove trailing whitespace in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Redirigir todas las solicitudes a index.html
-  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
+  },
+  server: {
+    historyApiFallback: true, // Redirigir todas las solicitudes a index.html
   },
 });
