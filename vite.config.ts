ccb6f5103699f51e7ac4ff1200740c8c1a63b98d
StefanoIG <stefano20033@gmail.com>
2024-10-12 15:06:53 -0500

    Refactor Vite config to remove manualChunks option and update outDir in build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    middlewareMode: 'html', // Redirigir todas las solicitudes a index.html
-  },
-});
+})
